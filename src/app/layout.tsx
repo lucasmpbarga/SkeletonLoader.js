c8f61506b60b312b6fcd53ccce2ed4101b6e231c
Lucas Barga <lucasmpbarga@gmail.com>
2024-11-03 11:46:22 -0300

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,12 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
